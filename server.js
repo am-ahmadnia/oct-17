3805505ed257a276c27d0a8cd79f3c26c84bbfad
Amirabas Ahmadnia <amahmadnia@outlook.com>
2020-10-17 13:06:03 +0330

    second copmmit

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
-const express = require("express");
-const app = express();
+// const express = require("express");
+// const app = express();
 
-const port = 3000;
+// const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
+
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`);
+// });
+
+const http = require("http");
+
+const hostname = "0.0.0.0";
+const port = 5000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Sysmon App is Up and Running!\n");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
